86028b286c47b6f979024893dc8afb009a393a20
pierreweb <16399775+pierreweb@users.noreply.github.com>
2025-05-28 21:34:38 +0200

    Update shader5.js

--- scripts/shader5.js
+++ scripts/shader5.js
@@ -34,18 +34,25 @@ function createProgram(gl, vs, fs) {
 
 function loadTexture(gl, url) {
     const texture = gl.createTexture();
-    gl.bindTexture(gl.TEXTURE_2D, texture);
-    
     const image = new Image();
     image.src = url;
     image.onload = function() {
         gl.bindTexture(gl.TEXTURE_2D, texture);
         gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+        
+        // Paramètres pour gérer les textures non carrées
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+
         gl.generateMipmap(gl.TEXTURE_2D);
     };
-    
     return texture;
 }
+
+
+
 
 // Charge une icône (remplace "icon.png" par ton image)
 const texture = loadTexture(gl, "/assets/icone.jpeg");
